d7eab983c223ae4b72e29607294ad4870649b8c1
oriest <oriestdjelloh@gmail.com>
2023-01-08 13:30:31 +0000

    make it

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ route(server);
 server.listen(process.env.PORT || PORT,async(req, res) => {
     // fixture.participantFixture();
     console.log("serveur exécuter sur http://localhost:" + PORT)
-    mongoose.connect('mongodb://localhost:27017/Ae2iGamer')
+    // mongoose.connect('mongodb://localhost:27017/Ae2iGamer')
+    mongoose.connect('mongodb+srv://oriesta:azerty77@cluster0.iczkn.mongodb.net/Ae2iGamer?retryWrites=true&w=majority')
     mongoose.connection
         .once('open', () => console.log('Connexion est établie'))
         .on('error', (error) => {
